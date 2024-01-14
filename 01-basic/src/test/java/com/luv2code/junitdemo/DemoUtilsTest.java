0406c53c6e08a4ca484275698991aa37596366f5
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2024-01-14 21:08:32 +0700

    assertNull and assertNotNull

--- 01-basic/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 01-basic/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -19,4 +19,18 @@ class DemoUtilsTest {
         assertEquals(expected, demoUtils.add(1, 5), "1 + 5 must equals to 6");
         assertNotEquals(unexpected, demoUtils.add(1, 5), "1 + 5 must not equals to 10");
     }
+
+    @Test
+    void checkNull() {
+        // given
+        DemoUtils demoUtils = new DemoUtils();
+
+        // when
+        String str1 = null;
+        String str2 = "Hello World";
+
+        // then
+        assertNull(str1, "Obj is null");
+        assertNotNull(str2, "Obj is not null");
+    }
 }
