92d0b037cb9f105a74b3cf143c896e347d1affe3
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2024-01-17 05:12:18 +0700

    @MockBean and @Autowired

--- 04-mockito/src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ 04-mockito/src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,10 +29,12 @@ public class MockAnnotationTest {
     @Autowired
     StudentGrades studentGrades;
 
-    @Mock
+//    @Mock
+    @MockBean
     private ApplicationDao applicationDao;
 
-    @InjectMocks
+//    @InjectMocks
+    @Autowired
     private ApplicationService applicationService;
 
     @BeforeEach
